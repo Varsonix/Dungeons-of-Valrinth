d678a2c5df44bb4ec848f40b1451b4b14e384652
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2024-02-22 14:34:29 -0800

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RLNET;
+
+namespace Dungeons_of_Valrinth
+{
+    public class Game
+    {
+        private static readonly int _screenWidth = 100;
+        private static readonly int _screenHeight = 70;
+
+        private static RLRootConsole _rootConsole;
+        public static void Main()
+        {
+            // This must be the exact name of the bitmap font file we are using or it will error.
+            string fontFileName = "terminal8x8.png";
+
+            // The title will appear at the top of the console window
+            string consoleTitle = "Dungeons of Valrinth - Level 1";
+
+            // Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels.
+            _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, consoleTitle);
+
+            // Set up a handler for RLNET's Update event
+            _rootConsole.Update += OnRootConsoleUpdate;
+
+            // Set up a handler for RLNET's Render event
+            _rootConsole.Render += OnRootConsoleRender;
+
+            // Begin RLNET's Game Loop
+            _rootConsole.Run();
+        }
+
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+        {
+            _rootConsole.Print(10, 10, "It worked!", RLColor.White);
+        }
+
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            _rootConsole.Draw();
+        }
+    }
+}
